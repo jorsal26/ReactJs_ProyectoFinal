365a52aee7101b50f01786cf6b0a8a1a290b63dc
jorsal26 <salinasjorgeluis26@gmail.com>
2024-07-21 22:57:26 -0300

    Presentacion del Proyecto Final de REACT-JS revision sobre path de imagenes de productos

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -17,7 +17,7 @@ const Carrito = () => {
                 {
                     carrito.map((prod) => (
                         <section className="grid gap-4 grid-cols-2 p-2 text-block bg-blue-100 rounded-md m-2" key={prod.id}>
-                                    <img className="w-50 h-50 rounded-md m-2 shadow-2xl" src={`/src/images/${prod.imagen}`} alt={prod.nombre} />
+                                    <img className="w-50 h-50 rounded-md m-2 shadow-2xl" src={`/${prod.imagen}`} alt={prod.nombre} />
                                     <h2 className="p-2 font-bold">{prod.nombre}</h2>
                                     <p>Cantidad: {prod.cantidad}</p>
                                     <p>Precio unitario: $ {prod.precio}</p>
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,7 +5,7 @@ function Item({producto, indice}) {
     return (
         <section className="grid gap-10 grid-cols-[repeat(auto-fill,minmax(200px,1fr))]">
             <article className="p-4 text-black transition-all bg-white rounded-md shadow-xl hover:scale-105">
-                <img src={`./src/images/${producto.imagen}`} alt={producto.nombre} className="object-contain aspect-square" />
+                <img src={`/${producto.imagen}`} alt={producto.nombre} className="object-contain aspect-square" />
                 <h2 className="font-bold">{producto.nombre}</h2>
                 <p className="text-green-400 font-bold" >${producto.precio}</p>
                 <Link to={`/item/${producto.id}`} className="verdetalle">ver detalle</Link>
